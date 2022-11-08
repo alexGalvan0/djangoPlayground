cbef7fa093d8726637eb358618cd89595326f178
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-08 20:58:32 +0000

    update

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Author, Book
+from pprint import pprint as pp
 
 # Create your views here.p
+
+
 def index(request):
     if request.method == 'GET':
-        return HttpResponse('<h1>Hello Wolrd</h1>')
+        authors = list(Author.objects.values())
+        return JsonResponse({'data': authors})
+
+
+def authors(request, id):
+    if request.method == 'GET':
+        authors = list(Book.objects.filter(id=id).values())
+        return JsonResponse({'data': authors})
+
+def authBook(request, name):
+    if request.method == 'GET':
+        authorId = list(Author.objects.filter(first_name = name))
+        pp(authorId[0])
+
+        books = list(Book.objects.filter(author = authorId[0].id).values())
+        return JsonResponse({'data':books})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('myapp.urls'))
+    path('book/', include('myapp.urls'))
 ]
